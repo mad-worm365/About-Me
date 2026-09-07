3796281b9a92b76385d555f70fa573d66700d3a6
howard-drive-digital <jonny.steven.js@gmail.com>
2026-09-07 15:33:52 +0200

    add: under-font

--- app/page.tsx
+++ app/page.tsx
@@ -14,6 +14,7 @@ import { Projects } from "@/components/Projects";
 import { Skills } from "@/components/Skills";
 import { Stats } from "@/components/Stats";
 import { Testimonials } from "@/components/Testimonials";
+import { WarpCTA } from "@/components/WarpCTA";
 
 export default function Home() {
   return (
@@ -24,6 +25,7 @@ export default function Home() {
         <Navbar />
         <main>
           <Hero />
+          <WarpCTA />
           <Stats />
           <About />
           <Skills />
